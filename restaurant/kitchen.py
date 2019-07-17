1a239cbe9541c52eb12a1fd1f8d606c90571e5d4
Edward <edward.preweda@agh.edu.pl>
2019-07-17 12:21:04 +0200

    Restauracja na nowym branchu

--- restaurant/kitchen.py
+++ restaurant/kitchen.py
@@ -1,10 +1,32 @@
+from time import time, sleep
+
+
 class Kitchen:
+
+    @staticmethod
+    def log(message):
+        with open('kitchen.log', 'a', encoding='utf-8')as f:
+            f.write(message +'\n')
 
     def __init__(self, manager):
         self.manager = manager
 
-    def prepare_meal(self):
-        pass
+    def prepare_meal(self, order):
+        self.log('Kuchnia: rozpoczynam przygotowanie')
+        self.__del()
 
-    def meal_ready(self):
-        pass
+        sleep(3)
+        self.log('z Kuchnia gotowe!')
+        self.meal_ready(order)
+
+    def meal_ready(self, order):
+        order.ready_at = time()
+        self.manager.meal_ready(order)
+
+
+    def log(message):
+        with open('kitchen.log', 'a', encoding='utf-8') in f:
+            return f.write(message + '\n')
+
+    def __del(self):
+        f.close
